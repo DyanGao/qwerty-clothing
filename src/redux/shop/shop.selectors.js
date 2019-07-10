72106752a406b04b70acd806f188be11d2601983
Dy <gdy1218@gmail.com>
2019-07-10 12:54:16 +0200

    data flow in app

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,10 +5,15 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
-)
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShop],
+  collections => Object.keys(collections).map(key => collections[key])
+);
 
 export const selectCollection = collectionUrlParam =>
  createSelector(
    [selectCollections],
    collections => collections[collectionUrlParam]
- )
+ );
