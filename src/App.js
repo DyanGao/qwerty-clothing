22e9a1e54b23d190afffe0f4a8edc639440038f3
Dy <gdy1218@gmail.com>
2019-07-23 15:55:39 +0200

    email sign in into sagas

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 
@@ -21,7 +18,6 @@ class App extends React.Component {
   unsubscribeFormAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
 
     // this.unsubscribeFormAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -71,11 +67,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(
-  mapStateToProps, 
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps)(App);
