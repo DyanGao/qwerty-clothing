802617c60959a4e71602e41337364a7f665b7cb0
Dy <gdy1218@gmail.com>
2019-07-26 09:29:55 +0200

    connected client to server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey ='pk_test_DZcFirtkex3eVGdZwttjQVPM00C218qiTE';
 
 const onTocken = token => {
-    console.log(token);
-    alert('Payment Successful');
-  }
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please make sure you use the provided credit cart.'
+      );
+    });
+  };
 
   return (
     <StripeCheckout
